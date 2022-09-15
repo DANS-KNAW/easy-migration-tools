fd95b1328f6d832401cd82a7fe446f5dfc56c981
jo-pol <jo-pol@users.noreply.github.com>
2022-09-15 10:56:45 +0200

    read/parse files.xml

--- src/easymigration/scripts/list_bagstore_files.py
+++ src/easymigration/scripts/list_bagstore_files.py
@@ -12,49 +12,46 @@ from easymigration.config import init
 from easymigration.pids_handling import non_empty_lines
 
 
-def find_files(doi, dark_url, csv_writer):
-    uuid = find_uuid(doi, dark_url)
-    files_xml = get_files_xml(uuid, dark_url)
+def find_files(doi, dark_archive, csv_writer):
+    # TODO continue batch if not found
+    uuid = find_uuid(doi, dark_archive["index_url"])
+    files_xml = get_files_xml(uuid, dark_archive["store_url"])
     parse_files_xml(doi, files_xml, csv_writer)
 
 
 def find_uuid(doi, bag_index_url):
     # locate in bag-index
     try:
-        params = {'doi': doi}
-        response = requests.get(
-            bag_index_url + 'search',
-            params=params)
+        params = {"doi": doi}
+        response = requests.get(f"{bag_index_url}/search", params=params)
         response.raise_for_status()
     except requests.exceptions.RequestException as re:
-        print("RequestException: ", re)
+        logging.error("RequestException: ", re)
         raise
-    resp_data = response.json()['result'][0]["bag-info"]["urn"]
+    resp_data = response.json()["result"][0]["bag-info"]["bag-id"]
     return resp_data
 
 
-def get_files_xml(urn, bag_store_url):
+def get_files_xml(uuid, bag_store_url):
     try:
-        url = "{}/bag-store/bags/{}/metadata/files.xml".format(bag_store_url, urn)
+        url = f"{bag_store_url}/bags/{uuid}/metadata/files.xml"
         logging.debug(url)
-        response = requests.put(url)
+        response = requests.get(url)
         response.raise_for_status()
     except requests.exceptions.RequestException as re:
-        print("RequestException: ", re)
+        logging.error("RequestException: ", re)
         raise
-    return response
+    return response.text
 
 
 def parse_files_xml(doi, files_xml, csv_writer):
-    mydoc = minidom.parse(files_xml)
-
-    file_items = mydoc.getElementsByTagName('file')
+    file_items = minidom.parseString(files_xml).getElementsByTagName("file")
     for elem in file_items:
-        access = elem.getElementsByTagName('accessibleToRights')
-        access_text = ''
+        access = elem.getElementsByTagName("accessibleToRights")
+        access_text = ""
         if access is not None and len(access) == 1:
             access_text = access[0].firstChild.nodeValue
-        row = {"doi": doi, "path": elem.attributes['filepath'].value, "accessCategory": access_text}
+        row = {"doi": doi, "path": elem.attributes["filepath"].value, "accessCategory": access_text}
         csv_writer.writerow(row)
 
 
@@ -62,26 +59,26 @@ def main():
     config = init()
     parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-        description='For each doi, list the files in the bag-store'
+        description="For each doi, list the files in the bag-store"
     )
-    parser.add_argument('-d', '--doi', dest='doi',
-                        help='Pid of a single dataset for which to find the files. '
-                             'When omitted, DOIs are read from stdin.')
+    parser.add_argument("-d", "--doi", dest="doi",
+                        help="Pid of a single dataset for which to find the files. "
+                             "When omitted, DOIs are read from stdin.")
     args = parser.parse_args()
 
-    dark_url = config['dark_archive']['base_url']
+    dark_archive = config["dark_archive"]
 
     fieldnames = {"doi", "path", "accessCategory"}
-    csv_writer = csv.DictWriter(sys.stdout, delimiter=',', fieldnames=fieldnames)
+    csv_writer = csv.DictWriter(sys.stdout, delimiter=",", fieldnames=fieldnames)
     csv_writer.writeheader()
 
     if args.doi is not None:
-        find_files(args.doi, dark_url, csv_writer)
+        find_files(args.doi, dark_archive, csv_writer)
     else:
         dois = non_empty_lines(sys.stdin.read())
         batch_process(dois,
-                      lambda doi: find_files(doi, dark_url, csv_writer))
+                      lambda doi: find_files(doi, dark_archive, csv_writer))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
