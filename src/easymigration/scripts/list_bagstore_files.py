47d51063eb4d053ca169889457f038c547749873
jo-pol <jo-pol@users.noreply.github.com>
2022-09-19 16:06:41 +0200

    uuid input

--- src/easymigration/scripts/list_bagstore_files.py
+++ src/easymigration/scripts/list_bagstore_files.py
@@ -12,46 +12,63 @@ from easymigration.config import init
 from easymigration.pids_handling import non_empty_lines
 
 
-def find_files(doi, dark_archive, csv_writer):
-    # TODO continue batch if not found
-    uuid = find_uuid(doi, dark_archive["index_url"])
-    logging.debug(f"DOI = {doi}; UUID={uuid}")
-    if uuid:
-        files_xml = get_files_xml(uuid, dark_archive["store_url"])
-        if (files_xml):
-            parse_files_xml(doi, files_xml, csv_writer)
+def find_files(uuid, dark_archive, csv_writer):
+    logging.debug(uuid)
+    store_url = dark_archive["store_url"]
+    metadata_url = f"{store_url}/bags/{uuid}/metadata"
+    files_xml = get_file(f"{metadata_url}/files.xml")
+    if (files_xml):
+        ddm = get_file(f"{metadata_url}/dataset.xml")
+        if (ddm):
+            doi = find_doi(ddm)
+            if doi:
+                parse_files_xml(uuid, doi, files_xml, csv_writer)
+            else:
+                logging.error(f"No DOI found for {uuid}")
 
 
 def find_uuid(doi, bag_index_url):
     # locate in bag-index
     response = requests.get(f"{bag_index_url}/search", params={"doi": doi})
     if response.status_code == 404:
-        logging.error(f"${doi} not available: {response.status_code}")
+        logging.error(f"{doi} not available: {response.status_code}")
         return ""
     elif response.status_code != 200:
-        raise Exception(f"${doi}: ${response.status_code}")
+        raise Exception(f"{doi}: {response.status_code}")
     return response.json()["result"][0]["bag-info"]["bag-id"]
 
 
-def get_files_xml(uuid, bag_store_url):
-    response = requests.get(f"{bag_store_url}/bags/{uuid}/metadata/files.xml")
-    if response.status_code == 410:
-        logging.error(f"Files for {uuid} not available: {response.status_code}")
+def get_file(url):
+    logging.debug(url)
+    response = requests.get(url)
+    if response.status_code == 410 or response.status_code == 404:
+        logging.error(f"Not found {response.status_code} : {url}")
         return ""
     elif response.status_code != 200:
-        raise Exception(f"${uuid}: ${response.status_code}")
+        raise Exception(f"status {response.status_code} : {url}")
     else:
         return response.text
 
 
-def parse_files_xml(doi, files_xml, csv_writer):
+def find_doi(ddm):
+    identifier_items = minidom.parseString(ddm).getElementsByTagNameNS("http://purl.org/dc/terms/", "identifier")
+    for elem in identifier_items:
+        logging.debug(elem)
+        type = elem.getAttributeNS("http://www.w3.org/2001/XMLSchema-instance", "type")
+        logging.debug(f"type = {type}")
+        if type == "id-type:DOI":
+            return elem.firstChild.nodeValue
+    return ""
+
+
+def parse_files_xml(uuid, doi, files_xml, csv_writer):
     file_items = minidom.parseString(files_xml).getElementsByTagName("file")
     for elem in file_items:
         access = elem.getElementsByTagName("accessibleToRights")
         access_text = ""
         if access is not None and len(access) == 1:
             access_text = access[0].firstChild.nodeValue
-        row = {"doi": doi, "path": elem.attributes["filepath"].value, "accessCategory": access_text}
+        row = {"uuid": uuid, "doi": doi, "path": elem.attributes["filepath"].value, "accessCategory": access_text}
         csv_writer.writerow(row)
 
 
@@ -61,23 +78,23 @@ def main():
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
         description="For each doi, list the files in the bag-store"
     )
-    parser.add_argument("-d", "--doi", dest="doi",
-                        help="Pid of a single dataset for which to find the files. "
-                             "When omitted, DOIs are read from stdin.")
+    parser.add_argument("-p", dest="pid",
+                        help="UUID of a bag for which to find the files. "
+                             "When omitted, UUIDs are read from stdin.")
     args = parser.parse_args()
 
     dark_archive = config["dark_archive"]
 
-    fieldnames = {"doi", "path", "accessCategory"}
+    fieldnames = {"uuid", "doi", "path", "accessCategory"}
     csv_writer = csv.DictWriter(sys.stdout, delimiter=",", fieldnames=fieldnames)
     csv_writer.writeheader()
 
-    if args.doi is not None:
-        find_files(args.doi, dark_archive, csv_writer)
+    if args.pid is not None:
+        find_files(args.pid, dark_archive, csv_writer)
     else:
-        dois = non_empty_lines(sys.stdin.read())
-        batch_process(dois,
-                      lambda doi: find_files(doi, dark_archive, csv_writer))
+        pids = non_empty_lines(sys.stdin.read())
+        batch_process(pids,
+                      lambda pid: find_files(pid, dark_archive, csv_writer))
 
 
 if __name__ == "__main__":
