d2a566d4709078392070c377128af076decf61a9
jo-pol <jo-pol@users.noreply.github.com>
2022-09-19 09:34:06 +0200

    error handling

--- src/easymigration/scripts/list_bagstore_files.py
+++ src/easymigration/scripts/list_bagstore_files.py
@@ -15,33 +15,33 @@ from easymigration.pids_handling import non_empty_lines
 def find_files(doi, dark_archive, csv_writer):
     # TODO continue batch if not found
     uuid = find_uuid(doi, dark_archive["index_url"])
-    files_xml = get_files_xml(uuid, dark_archive["store_url"])
-    parse_files_xml(doi, files_xml, csv_writer)
+    logging.debug(f"DOI = {doi}; UUID={uuid}")
+    if uuid:
+        files_xml = get_files_xml(uuid, dark_archive["store_url"])
+        if (files_xml):
+            parse_files_xml(doi, files_xml, csv_writer)
 
 
 def find_uuid(doi, bag_index_url):
     # locate in bag-index
-    try:
-        params = {"doi": doi}
-        response = requests.get(f"{bag_index_url}/search", params=params)
-        response.raise_for_status()
-    except requests.exceptions.RequestException as re:
-        logging.error("RequestException: ", re)
-        raise
-    resp_data = response.json()["result"][0]["bag-info"]["bag-id"]
-    return resp_data
+    response = requests.get(f"{bag_index_url}/search", params={"doi": doi})
+    if response.status_code == 404:
+        logging.error(f"${doi} not available: {response.status_code}")
+        return ""
+    elif response.status_code != 200:
+        raise Exception(f"${doi}: ${response.status_code}")
+    return response.json()["result"][0]["bag-info"]["bag-id"]
 
 
 def get_files_xml(uuid, bag_store_url):
-    try:
-        url = f"{bag_store_url}/bags/{uuid}/metadata/files.xml"
-        logging.debug(url)
-        response = requests.get(url)
-        response.raise_for_status()
-    except requests.exceptions.RequestException as re:
-        logging.error("RequestException: ", re)
-        raise
-    return response.text
+    response = requests.get(f"{bag_store_url}/bags/{uuid}/metadata/files.xml")
+    if response.status_code == 410:
+        logging.error(f"Files for {uuid} not available: {response.status_code}")
+        return ""
+    elif response.status_code != 200:
+        raise Exception(f"${uuid}: ${response.status_code}")
+    else:
+        return response.text
 
 
 def parse_files_xml(doi, files_xml, csv_writer):
